6489687c68bfe0720531535bbc8571b67c42d1bc
BMPMS <milesbryony@googlemail.com>
2025-04-08 17:36:07 +0100

    fix: draft1 updates

---
+++ js/dynamicTree.js
@@ -0,0 +1,290 @@
+
+const drawDynamicTree =  (divId, tooltipId, nodeHierarchy, links, radiusAttributes, colorAttributes) => {
+
+    const props = {
+        defaultRadius: 5,
+        radiusRange: [5,40],
+        links: {strokeWidth: 1, stroke:"#A0A0A0"},
+        nodes: {strokeWidth: 0.5,stroke: "#484848"},
+        label: {fill:"#484848",fontSize:10}
+    }
+
+    // formatData will return
+    // list of nodes + links
+    // colorAttributes -  can be used for colorPies
+    // radiusAttributes - can be used for radius size
+    // adding any additional string (color) or number (radius) attributes to your gephi dataset
+    // should automatically add them to the list AND aggregate them in the data
+
+
+    // TO DO
+    // add labels (final children only) - added for all, can easily hide
+    // zoom to fit - not quite right
+    // add color pies
+    // hook up color pies
+
+    const chartDiv = document.getElementById(divId);
+    const width = chartDiv.clientWidth;
+    const height = chartDiv.clientHeight;
+
+    let svg = d3.select(`#${divId}`).select("svg");
+    if (svg.node() === null) {
+        const base = d3.select(`#${divId}`).append("svg").attr("class","baseSvg");
+        svg = base.append("g").attr("class","baseSvg")
+        svg.append("g").attr("class", "linkGroup");
+        svg.append("g").attr("class", "nodeGroup");
+    }
+    const baseSvg = d3.select(".baseSvg");
+    baseSvg.attr("width",width).attr("height",height);
+    svg.attr("transform",`translate(${0},${0})`)
+
+    const zoom = d3
+        .zoom()
+        .scaleExtent([0.1,1])
+        .on("zoom", (event) => {
+            const { x, y, k } = event.transform;
+            svg.attr("transform", `translate(${x},${y}) scale(${k})`);
+
+        });
+
+    baseSvg.call(zoom).on("dblclick.zoom", null);
+
+    let radiusVar = radiusAttributes[0] ;
+    let colorVar = colorAttributes[0];
+
+    let radiusExtent = d3.extent(nodeHierarchy.descendants(), (d) => d.data[radiusVar]);
+
+
+    d3.select(".radiusSelect")
+        .selectAll("option")
+        .data(radiusAttributes)
+        .enter().append("option")
+        .attr("value", (d) => d)
+        .text((d) => d);
+
+    d3.select(".colorSelect")
+        .selectAll("option")
+        .data(colorAttributes)
+        .enter().append("option")
+        .attr("value", (d) => d)
+        .text((d) => d);
+
+    const radiusScale =  d3
+        .scaleSqrt()
+        .domain(radiusExtent)
+        .range(props.radiusRange);
+
+    const getColorSet = (colorVar) => Array.from(nodeHierarchy.descendants().reduce((acc, node) => {acc.add(node.data[colorVar]); return acc}, new Set()))
+    let colorDomain = getColorSet("defaultColor");
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain(colorDomain);
+
+    const linkGroup = svg.select(".linkGroup");
+    const nodeGroup = svg.select(".nodeGroup");
+
+    const chartNodes = nodeHierarchy.copy();
+
+    chartNodes.descendants()
+        .forEach((d) => {
+            if(d.depth === 0){ // fixing root node to centre
+                d.fx = width/2;
+                d.fy = height/2;
+            }
+            if(d.children){
+                if(d.depth >= 0){
+                    d.data._children = d.children;
+                    d.children = undefined;
+                }
+            }
+        });
+
+
+    const getRadius = (d) => d.children ? props.defaultRadius : radiusScale(d.data[radiusVar]);
+
+    d3.select(".radiusSelect")
+        .on("change", (event) => {
+            const value = event.currentTarget.value;
+            radiusVar = value;
+            radiusExtent = d3.extent(nodeHierarchy.descendants(), (d) => d.data[radiusVar]);
+            radiusScale.domain(radiusExtent);
+            svg.selectAll(".nodeCircle")
+                .transition()
+                .duration(400)
+                .attr("r", getRadius);
+
+            svg.selectAll(".nodeLabel")
+                .transition()
+                .duration(400)
+                .attr("dy", (d) => props.label.fontSize + getRadius(d))
+        })
+
+
+    const simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id((d) => d.data.name))
+        .force("radial", d3.forceRadial(d => d.depth * (width/2), width / 2, height / 2))
+        .force("collide", d3.forceCollide().radius((d) => getRadius(d) * 5).strength(1).iterations(6))
+
+    const getLinkId = (link, linkType) => typeof link[linkType] === "string" ? link[linkType] : link[linkType].data.name;
+
+    const fixNodes = (nodes) => {
+        nodes.map((m) => {
+            if(m.depth > 0){
+                m.fx = m.x;
+                m.fy = m.y;
+            }
+
+        })
+    }
+    const drawTree = () => {
+
+        const nodes = chartNodes.descendants();
+        fixNodes(nodes);
+        const chartLinks = links.filter((f) => nodes.some((s) => s.data.name === getLinkId(f, "source")) && nodes.some((s) => s.data.name === getLinkId(f,"target")))
+
+        const linksGroup = linkGroup
+            .selectAll(".linksGroup")
+            .data(chartLinks, (d) => d.key)
+            .join((group) => {
+                const enter = group.append("g").attr("class", "linksGroup");
+                enter.append("line").attr("class", "linkLine");
+                return enter;
+            });
+
+        linksGroup
+            .select(".linkLine")
+            .attr("stroke-width",props.links.strokeWidth)
+            .attr("stroke", props.links.stroke);
+
+        const nodesGroup = nodeGroup
+            .selectAll(".nodesGroup")
+            .data(nodes, (d) => d.data.key)
+            .join((group) => {
+                const enter = group.append("g").attr("class", "nodesGroup");
+                enter.append("circle").attr("class", "nodeCircle");
+                enter.append("rect").attr("class","nodeLabelBackground")
+                enter.append("text").attr("class", "nodeLabel");
+                enter.append("g").attr("class","nodePieGroup");
+                return enter;
+            });
+
+        nodesGroup
+            .attr("cursor","pointer")
+            .on("mouseover", (event,d) => {
+                d3.select(`#${tooltipId}`)
+                    .style("visibility", "visible")
+                    .html(getTooltipHtml(d,colorVar, colorScale, d.depth > 0 && colorVar === colorAttributes[0]))
+            })
+            .on("mouseout", () => {
+                d3.select(`#${tooltipId}`)
+                    .style("visibility", "hidden")
+                    .html("")
+            })
+            .on("click", (event, d) => {
+            if(!d.children  && d.data._children){
+                d.children = d.data._children;
+                d.data._children = undefined;
+            } else if (d.children !== undefined){
+                d.data._children = d.children;
+                d.children = undefined;
+            }
+            drawTree();
+        })
+
+        nodesGroup
+            .select(".nodeCircle")
+            .attr("r", getRadius)
+            .attr("fill", (d) => d.depth === 0 ? "#A0A0A0" : colorScale(d.data.defaultColor))
+            .attr("stroke", props.nodes.stroke)
+            .attr("stroke-width", props.nodes.strokeWidth)
+
+        nodesGroup
+            .select(".nodeLabelBackground")
+            .attr("width", (d) => measureWidth(d.data.label,props.label.fontSize) + 2)
+            .attr("height", props.label.fontSize)
+            .attr("x",(d) => -measureWidth(d.data.label,props.label.fontSize)/2 - 1)
+            .attr("y",(d) =>  getRadius(d) + 1.5)
+            .attr("rx",3)
+            .attr("ry",3)
+            .attr("fill","white");
+
+        nodesGroup
+            .select(".nodeLabel")
+            .attr("fill", props.label.fill)
+            .attr("font-size",props.label.fontSize)
+            .attr("dx", 0)
+            .attr("dy", (d) => props.label.fontSize + getRadius(d))
+            .attr("text-anchor", "middle")
+            .text((d) =>d.data.label);
+
+        const nodePiesGroup = nodesGroup.select(".nodePieGroup")
+            .selectAll(".nodePiesGroup")
+            .data( (d) => typeof d.data[colorVar] === "string" || colorVar === colorAttributes[0]? [] :
+                d3.pie().value((v) => v.count)(d.data[colorVar]).reduce((acc, entry) => {
+                    const radius = getRadius(d);
+                    const path = d3.arc().innerRadius(0).outerRadius(radius)(entry);
+                    acc.push({type: entry.data.type, path, radius});
+                    return acc;
+                },[]))
+            .join((group) => {
+                const enter = group.append("g").attr("class", "nodePiesGroup");
+                enter.append("path").attr("class", "piePath");
+                return enter;
+            });
+
+            nodePiesGroup.select(".piePath")
+                .attr("fill",(d) => colorScale(d.type))
+                .attr("d", (d) => d.path)
+
+        const dragstarted = (event, d) => {
+            if(d.depth > 0) {
+                fixNodes(nodes);
+                if (!event.active) simulation.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            }
+        };
+
+        const dragged = (event, d) => {
+            if(d.depth > 0) {
+                d.fx = event.x;
+                d.fy = event.y;
+            }
+        };
+
+        const dragended = (event, d) => {
+            if (!event.active) simulation.alphaTarget(0);
+        };
+        nodesGroup.call(
+            d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
+        );
+
+        simulation.nodes(nodes);
+        simulation.force("link").links(chartLinks);
+        simulation.alpha(1).restart();
+
+        simulation.on("tick", () => {
+            svg
+                .selectAll(".linkLine")
+                .attr("x1", (d) => d.source.x)
+                .attr("x2", (d) => d.target.x)
+                .attr("y1", (d) => d.source.y)
+                .attr("y2", (d) => d.target.y);
+
+            nodesGroup.attr("transform", (d) => `translate(${d.x},${d.y})`);
+        })
+
+        zoomToBounds(nodes, baseSvg,width,height,zoom);
+
+
+    }
+
+   drawTree();
+
+    d3.select(".colorSelect")
+        .on("change", (event) => {
+            const value = event.currentTarget.value;
+            colorVar = value;
+            colorDomain = getColorSet(colorVar);
+            colorScale.domain(colorDomain);
+            drawTree();
+        })
+}
---
+++ js/dynamicTreeFunctions.js
@@ -0,0 +1,223 @@
+const formatData = (chartData,nodeSpecificAttributes, defaultColorVar) => {
+    // formatting data so that
+    // a) node attributes can be toggled to represent size
+    // b) node attributes can be toggled to represent color
+    // c) parent nodes can have color pie charts (if time)
+
+    // no nodes = invalid data, return nothing
+    if(chartData.nodes.length === 0)return {nodes: [],links:[]};
+    // removing color, x and y from gephi data as not used
+    const nodeAttributes = Object.keys(chartData.nodes[0].attributes)
+        .filter((f) => f !== "color"  && f !== "x" && f !== "y");
+
+
+    // root = only node with no target
+    const root = chartData.nodes.filter((f) => !chartData.edges.some((s) => s.target === f.key));
+    if(root.length > 1){
+        // if more than one root = invalid data, returning nothing
+        console.error('Data Error: more than one root');
+        return {nodes: [],links:[]};
+    }
+
+    // building the hierarchy
+    const getChildren = (childKeys) =>
+        // loop through keys
+        childKeys.reduce((acc, key) => {
+            // find their child keys (ie source === key)
+            const leaveKeys = chartData.edges
+                .filter((f) => f.source === key)
+                .map((m) => m.target);
+            // repeat the process for their children (until end of tree)
+            const leaveChildren = getChildren(leaveKeys);
+            const child = {name: key};
+            if(leaveChildren.length > 0){
+                // add children if they exist - final leaf will have none
+                child.children = leaveChildren;
+            }
+            acc.push(child)
+            return acc;
+        },[])
+
+    const rootKey = root[0].key;
+    // branchKeys = source === root
+    const branchKeys = chartData.edges
+        .filter((f) => f.source === rootKey)
+        .map((m) => m.target);
+    // build hierarchy and convert to d3.hierarchy format (enables manipulation)
+    const hierarchy = d3.hierarchy({
+        name: rootKey,
+        children: getChildren(branchKeys)});
+    // aggregating the node totals up the hierarchy (for radius, colour and pies)
+
+    // for final level leaves only
+    const getNodeData = (nodeKey) => {
+        // find matching node
+        const matchingNode = chartData.nodes.find((f) => f.key === nodeKey);
+        const newNode = {name: matchingNode.key};
+        // loop through attributes and add to data
+        nodeAttributes.forEach((n) => newNode[n] = matchingNode.attributes[n]);
+        return newNode
+    }
+
+    // group descendants by depth
+    const descendantsByDepth = d3.group(hierarchy.descendants(), (g) => g.depth);
+    // add data for final level leaf nodes
+    const bottomLevelNodes = descendantsByDepth.get(3);
+    bottomLevelNodes.map((m) => {
+        m.data = getNodeData(m.data.name);
+    })
+
+    const radiusAttributes = new Set();
+    const colorAttributes = new Set();
+    // aggregation for parent nodes
+    const getNodeTotals = (nodeName, nodeChildren) => {
+        const newNode = {name: nodeName};
+        nodeAttributes.forEach((n) =>  {
+            if(nodeSpecificAttributes.includes(n)){
+                // for all nodeSpecificAttributes - just add data
+                const matchingNode = chartData.nodes.find((f) => f.key === nodeName)
+                newNode[n] = matchingNode.attributes[n];
+            } else if(typeof nodeChildren[0].data[n] === "number"){
+                // numbers are easy - simply aggregate
+                newNode[n] = d3.sum(nodeChildren,(s) => s.data[n]);
+                // add to radius attributes set
+                radiusAttributes.add(n);
+            } else {
+                // strings
+                // add to color attributes set
+                colorAttributes.add(n);
+                newNode[n] = Array.from(nodeChildren.reduce((acc, child) => {
+                    if(typeof child.data[n] === "string"){
+                        // level 2 (one up from final leaf)
+                        const currentType = child.data[n];
+                        const matchingType = acc.find((f) => f.type === currentType);
+                        // either add or aggregate depending on whether it exists already
+                        if(matchingType){
+                            matchingType.count += 1;
+                        } else {
+                            acc.push({type: currentType, count: 1})
+                        }
+                    } else {
+                        if(acc.length === 0){
+                            // first instance === copy (deep cloned)
+                            acc = JSON.parse(JSON.stringify(child.data[n]));
+                            acc.forEach((a) => a.count = +a.count);
+                        } else {
+                            // then loop through
+                            child.data[n].forEach((c) => {
+                                const matchingType = acc.find((f) => f.type === c.type);
+                                // and either aggregate or add depending on whether it exists already
+                                if(matchingType){
+                                    matchingType.count += c.count;
+                                } else {
+                                    // it should exist already but in case there is missing data
+                                    acc.push({type: c.type, count: c.count});
+                                }
+                            })
+                        }
+                    }
+                    return acc;
+                },[]))
+
+            }
+        });
+        return newNode
+    }
+
+    // loop down through parent nodes and aggregate at each level;
+    for (let i = 2; i >= 0 ; i--){
+        const levelNodes = descendantsByDepth.get(i);
+        levelNodes.map((m) => {
+            m.data = getNodeTotals(m.data.name, m.children);
+        })
+    }
+
+    // update all descendants to include default color var from depth 1 descendant
+    hierarchy.descendants().forEach((d) => {
+        if(d.depth === 0) {
+            d.data.defaultColor = "root";
+        } else if (d.depth === 1){
+            d.data.defaultColor = d.data[defaultColorVar];
+        } else {
+            const depth1Ancestor = d.ancestors().find((f) => f.depth === 1);
+            d.data.defaultColor = depth1Ancestor.data[defaultColorVar];
+        }
+    })
+
+    // now calculations are complete, pass back nodes and links
+    const nodes = hierarchy.descendants().reduce((acc, node) => {
+        const newNode = {};
+        Object.keys(node.data).forEach((k) => newNode[k] = node.data[k]);
+        acc.push(newNode);
+        return acc;
+    },[]);
+
+    const links = chartData.edges.reduce((acc, edge) => {
+        acc.push({key: edge.key, source: edge.source, target: edge.target})
+        return acc;
+    },[])
+
+
+    return {nodeHierarchy: hierarchy, links, colorAttributes: Array.from(colorAttributes), radiusAttributes: Array.from(radiusAttributes)};
+
+}
+
+const measureWidth = (text, fontSize) => {
+    const context = document.createElement("canvas").getContext("2d");
+    context.font = `${fontSize}px Arial`;
+    return context.measureText(text).width;
+}
+
+
+const getTooltipHtml = (d, colorVar, colorScale, defaultColor) => {
+    const excludedKeys = ["label","defaultColor","_children","children","name"];
+    let tooltipHTML = `<span style="font-size: 18px; color:${defaultColor ? colorScale(d.data.defaultColor) : "#484848"};"><strong>${d.data.label.toUpperCase()}</strong></span><br><br><table style="width:100%;">`;
+    Object.keys(d.data).forEach((k) => {
+        if(!excludedKeys.includes(k)){
+            if(typeof d.data[k] === "object"){
+                tooltipHTML +=  `<tr><td class="cellLeft"><strong>${k.toUpperCase()} </strong></td><td class="cellRight">`
+                d.data[k].forEach((e) => {
+
+                    tooltipHTML += `<span style="color:${k === colorVar ? colorScale(e.type) : "#484848"}">${e.type}: ${e.count}</span><br>`
+                })
+                tooltipHTML += "</td></tr>"
+            } else {
+
+                tooltipHTML += `<tr><td class="cellLeft"><strong>${k.toUpperCase()}</strong></td><td class="cellRight">${d.data[k]}</td></tr>`
+            }
+
+        }
+    })
+    tooltipHTML += "</table>"
+    return tooltipHTML;
+}
+
+const zoomToBounds = (currentNodes, baseSvg, width, height,zoom) => {
+    const [xExtent0, xExtent1] = d3.extent(currentNodes, (d) => d.fx || d.x);
+    const [yExtent0, yExtent1] = d3.extent(currentNodes, (d) => d.fy || d.y);
+    if (xExtent0 && xExtent1 && yExtent0 && yExtent1) {
+        let xWidth = xExtent1 - xExtent0 + 40;
+        let yWidth = yExtent1 - yExtent0 + 40;
+        let translateX =  -(xExtent0 + xExtent1) / 2;
+        let translateY =  -(yExtent0 + yExtent1) / 2;
+
+        const fitToScale = 0.95 / Math.max(xWidth / width, yWidth / height);
+
+        if(fitToScale < 1.2){
+
+            baseSvg
+                .interrupt()
+                .transition()
+                .duration(500)
+                .call(
+                    zoom.transform,
+                    d3.zoomIdentity
+                        .translate(width / 2, height / 2)
+                        .scale(fitToScale)
+                        .translate(translateX, translateY),
+                );
+        }
+
+
+    }
+}
